a584939ed0e4666ebd8f5750494231af507f3246
Dawid Janczak <dawid.janczak@gmail.com>
2012-08-27 14:55:25 +0200

    Change xterm to uxterm.

--- rc.lua
+++ rc.lua
@@ -39,7 +39,7 @@ end
 beautiful.init("dotfiles/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "uxterm"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 wrling_cmd = terminal .. " -e ssh wrling33"
