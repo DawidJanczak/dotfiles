8c8d91e841de61f271bd74128b1e57b846f640a4
Dawid Janczak <dawid.janczak@gmail.com>
2022-03-16 17:08:09 +0800

    Update launch script

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,4 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
-polybar bar &
+echo "---" | tee -a /tmp/polybar.log
+polybar bar 2>&1 | tee -a /tmp/polybar.log & disown
+echo "Bars launched..."
